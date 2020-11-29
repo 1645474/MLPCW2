919fb36b9478125d4d5b3bbc88e970d950b09ca1
s1645474 <s1645474@sms.ed.ac.uk>
2020-11-29 22:55:21 +0000

    detach

--- pytorch_mlp_framework/experiment_builder.py
+++ pytorch_mlp_framework/experiment_builder.py
@@ -157,7 +157,7 @@ class ExperimentBuilder(nn.Module):
 
         for name, param in named_parameters:
             layers.append(name)
-            all_grads.append(np.mean(np.abs(param.cpu())))
+            all_grads.append(np.mean(np.abs(param.detach.numpy())))
         
         ########################################
             
