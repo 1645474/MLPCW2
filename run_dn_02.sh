d11fab22c0c7b738880149c6c13097759eb74df7
s1645474 <s1645474@sms.ed.ac.uk>
2020-12-01 16:58:57 +0000

    new script

---
+++ run_dn_02.sh
@@ -0,0 +1,1 @@
+python pytorch_mlp_framework/train_evaluate_image_classification_system.py --batch_size 100 --seed 0 --num_filters 16 --num_stages 1 --num_blocks_per_stage 2 --experiment_name DN_08_experiment --use_gpu True --num_classes 100 --block_type 'dense_block' --continue_from_epoch -1
