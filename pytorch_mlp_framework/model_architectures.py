28d811f581e5d64679f62e2625a1d6095bf9767d
s1645474 <s1645474@sms.ed.ac.uk>
2020-12-01 20:22:37 +0000

    int

--- pytorch_mlp_framework/model_architectures.py
+++ pytorch_mlp_framework/model_architectures.py
@@ -426,7 +426,7 @@ class ConvolutionalNetwork(nn.Module):
         # torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True)
         for i in range(self.num_stages):  # for number of layers times
             if self.processing_block_type == DenseBlock:
-                self.layer_dict['dense_block_{}'.format(i)] = self.processing_block_type(input_shape=out.shape, num_filters=(self.num_filters*(0.5**i)), bias=self.use_bias, kernel_size=3, dilation=1, padding=1, num_blocks_per_stage=self.num_blocks_per_stage)
+                self.layer_dict['dense_block_{}'.format(i)] = self.processing_block_type(input_shape=out.shape, num_filters=int(self.num_filters*(0.5**i)), bias=self.use_bias, kernel_size=3, dilation=1, padding=1, num_blocks_per_stage=self.num_blocks_per_stage)
             else:
                 for j in range(self.num_blocks_per_stage):
                     self.layer_dict['block_{}_{}'.format(i, j)] = self.processing_block_type(input_shape=out.shape, num_filters=self.num_filters, bias=self.use_bias, kernel_size=3, dilation=1, padding=1)
